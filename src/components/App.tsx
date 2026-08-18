de932b0565aba614d3df0266d7a19d1a4d232089
skycherepanya <skycherepanya@gmail.com>
2026-08-18 20:43:06 +0200

    refactor: move App component and styles into components directory and update configuration

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,13 +2,13 @@ import { useState } from "react";
 import { Toaster } from 'react-hot-toast';
 import css from "./App.module.css";
 
-import type { Movie } from "./src/types/movie";
-import fetchMovies from "./src/services/movieService";
-import SearchBar from "./src/components/SearchBar/SearchBar";
-import MovieGrid from "./src/components/MovieGrid/MovieGrid";
-import Loader from "./src/components/Loader/Loader";
-import ErrorMessage from "./src/components/ErrorMessage/ErrorMessage";
-import MovieModal from "./src/components/MovieModal/MovieModal";
+import type { Movie } from "../types/movie";
+import fetchMovies from "../services/movieService";
+import SearchBar from "./SearchBar/SearchBar";
+import MovieGrid from "./MovieGrid/MovieGrid";
+import Loader from "./Loader/Loader";
+import ErrorMessage from "./ErrorMessage/ErrorMessage";
+import MovieModal from "./MovieModal/MovieModal";
 
 export default function App() {
   const [movies, setMovies] = useState<Movie[]>([]);
